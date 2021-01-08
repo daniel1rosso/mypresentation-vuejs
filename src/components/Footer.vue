94de8eeb1d01f7e06fa82566945066370a01dcf2
Daniel Alberto Rosso <danielalbertorosso@hotmail.com>
2021-01-08 15:51:25 -0300

    Initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <div>
+  <footer class="site-footer">
+        <div class="site-container">
+            <div class="footer-inner" style="place-content: center;">
+                <div class="footer-info" style="width: auto;">
+                    <div class="footer-info__right" style="text-align: justify;">
+                        <h5 style="text-align: center;">Contactos</h5>
+                        <div class="social-icons">
+                            <p style="text-align: center;">
+                                Celular: +5493537512557
+                            </p>
+                            <p style="margin-bottom: 10%;text-align: center;">
+                                Email: danielalbertorosso@gmail.com
+                            </p>
+                            <div style="text-align: center;margin-left: 3%;">
+                                <a href="mailto:danielalbertorosso@gmail.com?Subject=Página%20Personal" target="_blank"><img src="images/email.webp" alt="social icon" style="width: 50%;" loading="lazy"></a>
+                                <a href="https://wa.link/tzstnj" target="_blank"><img src="images/whatsapp.webp" alt="social icon" style="width: 50%;" loading="lazy"></a>
+                                <a href="https://www.linkedin.com/in/daniel-alberto-rosso-444305170/" target="_blank"><img src="images/linkedin.webp" alt="social icon" style="width: 50%;" loading="lazy"></a>
+                                <a href="https://www.instagram.com/daniel_rosso_/?hl=es-la" target="_blank"><img src="images/instagram-sketched.webp" alt="social icon" style="width: 50%;" loading="lazy"></a>
+                                <a href="https://www.facebook.com/rocho13" target="_blank"><img src="images/facebook.webp" alt="social icon" style="width: 50%;" loading="lazy"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p style="text-align: center;color: white;">© <span id="aniopage"></span> | ROSSO, DANIEL ALBERTO</p>
+        </div>
+    </footer>
+  </div>
+
+</template>
---
+++ src/views/Ladding.vue
@@ -0,0 +1,259 @@
+<template>
+  <div>
+    <!-- Top banner -->
+    <!--<section class="fh5co-top-banner" style="height: 100vh;">
+        <div class="top-banner__inner site-container padtable">
+            <div class="top-banner__image">
+                <img src="images/top-banner-author.webp" alt="author image" loading="lazy">
+            </div>
+            <div class="top-banner__text">
+                <div class="top-banner__text-up">
+                    <span class="brand-span">Hello! I'm</span>
+                    <h2 class="top-banner__h2">Daniel A.</h2>
+                </div>
+                <div class="top-banner__text-down">
+                    <h2 class="top-banner__h2">Rosso</h2>
+                    <span class="brand-span">Desarrollador (Back-End) y estudiante de Ingeniería en Sistemas de Información</span>
+                </div>
+            </div>
+        </div>
+    </section>-->
+    <!-- Top banner end -->
+
+    <!-- About me -->
+    <section class="fh5co-about-me">
+        <div class="about-me-inner site-container">
+            <article class="portfolio-wrapper">
+                <div class="portfolio__img">
+                    <img src="images/about-me.webp" class="about-me__profile" alt="about me profile picture" loading="lazy">
+                </div>
+                <div class="portfolio__bottom">
+                    <div class="portfolio__name">
+                        <span>D</span>
+                        <h2 class="universal-h2">aniel A</h2>
+                        <span>R</span>
+                        <h2 class="universal-h2">osso</h2>
+                    </div>
+                    <p>Soy desarrollador de Back-End y un estudiante de Ingenieria en Sistemas de Información en UTN-FRVM.</p>
+                </div>
+            </article>
+            <div class="about-me__text">
+                <div class="about-me-slider">
+                    <div class="about-me-single-slide">
+                        <h2 class="universal-h2 universal-h2-bckg">Sobre Mí !</h2>
+                        <p><span>P</span>oseo experiencia laboral en diferentes proyectos, en los que he desplegado grandes habilidades, además de desempeñarme con dedicación, responsabilidad y entusiasmo al momento de analizar las problemáticas planteadas
+                            y de este modo, obtener las resoluciones más satisfactorias para las mismas.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="about-me-bckg"></div>
+    </section>
+    <!-- About me end -->
+
+    <!-- Blog -->
+    <section class="fh5co-blog">
+        <div class="site-container">
+            <h2 class="universal-h2 universal-h2-bckg">Proyectos en Chile</h2>
+            <div class="blog-slider blog-inner">
+                <div class="single-blog">
+                    <div class="single-blog__img">
+                        <img src="images/proyectos/sakiservice.webp" alt="blog image" loading="lazy">
+                    </div>
+                    <div class="single-blog__text">
+                        <h4>Saki Service</h4>
+                        <span>2020-Actualidad</span>
+                        <p>Servicio de encomiendas. Ubicado en Santiago de Chile, Chile</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Blog end -->
+
+    <!-- Blog -->
+    <section class="fh5co-blog">
+        <div class="site-container">
+            <h2 class="universal-h2 universal-h2-bckg">Proyectos en Argentina</h2>
+            <div class="blog-slider blog-inner">
+                <div class="single-blog">
+                    <div class="single-blog__img" style="max-width: 50%;">
+                        <img src="images/proyectos/iac-mutual.webp" alt="blog image" loading="lazy">
+                    </div>
+                    <div class="single-blog__text">
+                        <h4>IAC</h4>
+                        <span>2019-2020</span>
+                        <p>Independiente Atletic Club. Ubicado en Chañar Ladeado, Santa Fe, Argentina</p>
+                    </div>
+                </div>
+                <div class="single-blog">
+                    <div class="single-blog__img">
+                        <img src="images/proyectos/bisoft.webp" alt="blog image" loading="lazy">
+                    </div>
+                    <div class="single-blog__text">
+                        <h4>Bisoft</h4>
+                        <span>2019-2020</span>
+                        <p>Producto desarrollado para el desempeño de una empresa en su totalidad</p>
+                    </div>
+                </div>
+                <div class="single-blog">
+                    <div class="single-blog__img">
+                        <img src="images/proyectos/elproductorporcino.webp" alt="blog image" loading="lazy">
+                    </div>
+                    <div class="single-blog__text">
+                        <h4>El Productor Porcino</h4>
+                        <span>2019-2020</span>
+                        <p>Noticias, Empresas, Articulos, Eventos, entre otras cosas referidos al Mercado Porcino</p>
+                    </div>
+                </div>
+                <div class="single-blog">
+                    <div class="single-blog__img">
+                        <img src="images/proyectos/entretur.webp" alt="blog image" loading="lazy">
+                    </div>
+                    <div class="single-blog__text">
+                        <h4>Entretur</h4>
+                        <span>2019</span>
+                        <p>Empresa de turimo Nacionales e Internaciones. Ubicado en Paraná, Entre Ríos, Argentina</p>
+                    </div>
+                </div>
+                <div class="single-blog">
+                    <div class="single-blog__img">
+                        <img src="images/proyectos/newint.webp" alt="blog image" loading="lazy">
+                    </div>
+                    <div class="single-blog__text">
+                        <h4>Registro de Interesados</h4>
+                        <span>2018</span>
+                        <p>Formulario de registro de interesados relizado en UTN, Villa María, Córdoba, Argentina</p>
+                    </div>
+                </div>
+                <div class="single-blog">
+                    <div class="single-blog__img">
+                        <img src="images/proyectos/listadoInteresado.webp" alt="blog image" loading="lazy">
+                    </div>
+                    <div class="single-blog__text">
+                        <h4>Listados</h4>
+                        <span>2018</span>
+                        <p>Listados de interesados, graduados y alumanos a los talleres realizado en UTN, Villa María, Córdoba, Argentina</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Blog end -->
+
+    <!-- Divisor -->
+    <section class="fh5co-social counters" style="margin-bottom: 5%;">
+        <div class="site-container">
+            <h2 class="universal-h2 universal-h2-bckg">Portfolio GitHub</h2>
+            <h3 style="text-align: center;">Te invito a que lo visites haciendo click en el icono</h3>
+            <a href="https://github.com/daniel1rosso" target="_blank" style="text-align: center; display:block; margin-left: auto; margin-right: auto; margin-top: 2%;"><img src="images/github.webp" alt="GitHub" style="width: 25%;"></a>
+        </div>
+    </section>
+    <!-- Divisor end -->
+
+    <!-- Blog -->
+    <section class="fh5co-blog">
+        <div class="site-container">
+            <h2 class="universal-h2 universal-h2-bckg">Experiencia Laboral</h2>
+            <div class="blog-slider blog-inner">
+                <div class="single-blog">
+                    <div class="single-blog__img">
+                        <img src="images/logotele.webp" alt="blog image" loading="lazy">
+                    </div>
+                    <div class="single-blog__text">
+                        <h4>Telepathic Soft</h4>
+                        <span>From January 1, 2019 to July 31, 2020</span>
+                        <p>Duración de Experiencia: Un año y siete meses.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Blog end -->
+
+    <!-- Quotes -->
+    <section class="fh5co-quotes">
+        <div class="site-container">
+            <div class="about-me-slider">
+                <div style="text-align: justify;color:white;">
+                    <h2 class="universal-h2 universal-h2-bckg">Conocimientos de Desarrollo</h2>
+                    <p style="margin-bottom: 1%;">
+                        <span style="font-weight: bold;text-decoration: underline;">Gestor de configuraciones:</span> GIT (GitHub, GitLab)
+                    </p>
+                    <p style="margin-bottom: 1%;">
+                        <span style="font-weight: bold;text-decoration: underline;">Framework de desarrollo usado:</span> Codeigniter, Express (Basico), VueJS
+                    </p>
+                    <p style="margin-bottom: 1%;">
+                        <span style="font-weight: bold;text-decoration: underline;">Lenguajes de desarrollo:</span> PHP, JavaScript, HTML, CSS, NodeJs (Basica), VueJS
+                    </p>
+                    <p style="margin-bottom: 1%;">
+                        <span style="font-weight: bold;text-decoration: underline;">Esquema de base de datos:</span> SQL (MySQL, Postgress), NoSQL (MongoDB)
+                    </p>
+                    <p style="margin-bottom: 1%;">
+                        <span style="font-weight: bold;text-decoration: underline;">Arquitectura de desarrollo:</span> MVC
+                    </p>
+                    <p style="margin-bottom: 1%;">
+                        <span style="font-weight: bold;text-decoration: underline;">Desarrolos de sistemas, servicios y gestión por medio de API REST</span>
+                    </p>
+                    <p style="margin-bottom: 1%;">
+                        <span style="font-weight: bold;text-decoration: underline;">Manejo basico de servidores en Linux</span>
+                    </p>
+                    <p>
+                        <span style="font-weight: bold;text-decoration: underline;">CMS:</span> Wordpress
+                    </p>
+                </div>
+                <div style="text-align: justify;">
+                    <h2 class="universal-h2 universal-h2-bckg">Conocimientos Académicos</h2>
+                    <p style="margin-bottom: 1%;">
+                        <span style="font-weight: bold;text-decoration: underline;">Universidad:</span> Ingeniería en Sistemas de Información (Cursando en UTN, Villa María, Córdoba, Argentina)
+                    </p>
+                    <p style="margin-bottom: 1%;">
+                        Analista del Conocimiento - Trayecto Programador (Plan 111 mil - Recibido en Córdoba, Argentina)
+                    </p>
+                    <p style="margin-bottom: 1%;">
+                        Conv. Complemento y Asistencia Educativa (UTN, Villa María, Córdoba, Argentina)
+                    </p>
+                    <p style="margin-bottom: 1%;">
+                        Capacitación Reparación y mantenimiento de computadoras (Bell Ville, Córdoba, Argentina)
+                    </p>
+                    <p style="margin-bottom: 1%;">
+                        Información y tecnologías aprendidas en este lapso de tiempo fueron de autodidacta
+                    </p>
+                    
+                    <p style="margin-bottom: 1%;">
+                        <span style="font-weight: bold;text-decoration: underline;">Secundario:</span> Técnico de Nivel Medio en Equipos e Instalaciones Electromecánicas (Recibido en Bell Ville, Córdoba, Argentina)
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Quotes end -->
+
+    <!-- Social -->
+    <section class="fh5co-social" style="height: 2%;">
+        <div class="site-container">
+        </div>
+    </section>
+    <!-- Social -->
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name:"Ladding",
+  components:{
+    Footer
+  },
+  methods:{},
+  mounted () {},
+  optimization: {
+      splitChunks: {
+          chunks: 'all',
+      },
+  }
+}
+</script>
