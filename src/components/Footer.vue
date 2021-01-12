68d82aa2c5037e093efbeac2d6527cccf87f0ec3
Daniel Alberto Rosso <danielalbertorosso@hotmail.com>
2021-01-12 20:59:44 -0300

    Added Telegram contact

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,9 +14,10 @@
                             <p style="margin-bottom: 10%;text-align: center;">
                                 Email: danielalbertorosso@gmail.com
                             </p>
-                            <div style="text-align: center;margin-left: 3%;">
+                            <div style="text-align: center; margin-left: 3%; width: max-content;">
                                 <a href="mailto:danielalbertorosso@gmail.com?Subject=Página%20Personal" target="_blank"><img src="images/email.webp" alt="social icon" style="width: 50%;" loading="lazy"></a>
                                 <a href="https://wa.link/tzstnj" target="_blank"><img src="images/whatsapp.webp" alt="social icon" style="width: 50%;" loading="lazy"></a>
+                                <a href="https://t.me/danielrosso" target="_blank"><img src="images/telegram.webp" alt="social icon" style="width: 50%;" loading="lazy"></a>
                                 <a href="https://www.linkedin.com/in/daniel-alberto-rosso-444305170/" target="_blank"><img src="images/linkedin.webp" alt="social icon" style="width: 50%;" loading="lazy"></a>
                                 <a href="https://www.instagram.com/daniel_rosso_/?hl=es-la" target="_blank"><img src="images/instagram-sketched.webp" alt="social icon" style="width: 50%;" loading="lazy"></a>
                                 <a href="https://www.facebook.com/rocho13" target="_blank"><img src="images/facebook.webp" alt="social icon" style="width: 50%;" loading="lazy"></a>
